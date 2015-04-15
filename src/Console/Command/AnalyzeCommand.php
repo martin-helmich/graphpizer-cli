5f8764f3b0d28f3f5226d571e8f21efcf906862c
Martin Helmich <kontakt@martin-helmich.de>
2015-04-15 11:49:37 +0200

    Move command to different namespace

--- src/Console/Command/AnalyzeCommand.php
+++ src/Console/Command/AnalyzeCommand.php
@@ -1,8 +1,10 @@
 <?php
-namespace Helmich\Graphizer\Console;
+namespace Helmich\Graphizer\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class AnalyzeCommand extends Command {
 
@@ -10,10 +12,15 @@ class AnalyzeCommand extends Command {
 		$this
 			->setName('analyze')
 			->setDescription('Analyze a set of files')
-			->addOption('neoHost', 'h', InputArgument::OPTIONAL, 'Hostname of Neo4j server', 'localhost')
+			->addOption('neoHost', 'H', InputArgument::OPTIONAL, 'Hostname of Neo4j server', 'localhost')
 			->addOption('neoPort', 'P', InputArgument::OPTIONAL, 'Port of Neo4j server', 7474)
 			->addOption('neoUser', 'u', InputArgument::OPTIONAL, 'Username for Neo4j server', '')
 			->addOption('neoPassword', 'p', InputArgument::OPTIONAL, 'Password for Neo4j server', '')
+			->addOption('prune', NULL, InputArgument::OPTIONAL, 'Prune the database before execution')
 			->addArgument('dir', InputArgument::IS_ARRAY | InputArgument::REQUIRED);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$paths = $input->getArgument('dir');
 	}
 }
