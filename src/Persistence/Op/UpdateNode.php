fe4ececec4d0bc4186f7e7d9ae3736fa9c68eac6
Martin Helmich <m.helmich@mittwald.de>
2015-05-14 17:25:39 +0200

    Add license header and class doccomments

--- src/Persistence/Op/UpdateNode.php
+++ src/Persistence/Op/UpdateNode.php
@@ -1,6 +1,31 @@
 <?php
+
+/*
+ * GraPHPizer - Store PHP syntax trees in a Neo4j database
+ * Copyright (C) 2015  Martin Helmich <kontakt@martin-helmich.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Helmich\Graphizer\Persistence\Op;
 
+/**
+ * Operation that updates a given node
+ *
+ * @package    Helmich\Graphizer
+ * @subpackage Persistence\Op
+ */
 class UpdateNode implements Operation {
 
 	/**
