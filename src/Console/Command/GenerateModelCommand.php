5266d83afd885e7872d48125d8d4a0f3e30330af
Martin Helmich <m.helmich@mittwald.de>
2015-05-17 11:58:37 +0200

    Option to analyze package dependencies

--- src/Console/Command/GenerateModelCommand.php
+++ src/Console/Command/GenerateModelCommand.php
@@ -12,13 +12,14 @@ class GenerateModelCommand extends AbstractCommand {
 		$this
 			->setName('generate:model')
 			->setDescription('Generates a meta model from stored syntax trees')
-			->addOption('with-usage', NULL, InputOption::VALUE_NONE, 'Analyze class usages');
+			->addOption('with-usages', NULL, InputOption::VALUE_NONE, 'Infer class usages')
+			->addOption('with-dependencies', NULL, InputOption::VALUE_NONE, 'Analyze package dependencies');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$backend = $this->connect($input, $output);
 
 		$service = new ModelGenerationService($backend);
-		$service->generateModel($input->getOption('with-usage'));
+		$service->generateModel($input->getOption('with-usage'), $input->getOption('with-dependencies'));
 	}
 }
