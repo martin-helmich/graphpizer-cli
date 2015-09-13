4498047ddcf7335a2e991209af51de7da918af28
Martin Helmich <kontakt@martin-helmich.de>
2015-09-13 21:23:58 +0200

    Wipe single projects

--- src/Console/Command/PruneCommand.php
+++ src/Console/Command/PruneCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace Helmich\Graphizer\Console\Command;
 
+use Helmich\Graphizer\Configuration\ProjectConfiguration;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -9,12 +11,15 @@ class PruneCommand extends AbstractCommand {
 	protected function configure() {
 		$this
 			->setName('import:prune')
-			->setDescription('Clear the database of everything. Really, everything!');
+			->setDescription('Clear the database of everything. Really, everything!')
+			->addArgument('project', InputArgument::REQUIRED, 'The project to wipe', 'default');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
+		$project = new ProjectConfiguration($input->getArgument('project'), $input->getArgument('project'));
+
 		$backend = $this->connect($input, $output);
-		$backend->wipe();
+		$backend->wipe($project);
 
 		$output->writeln("Deleted <comment>everything</comment>. You happy now?");
 	}
